aaf22527a48c2d9d1337fdf1dea50658a3944915
Igor Conrado <conradoigor78@gmail.com>
2026-03-17 21:57:48 -0300

    chore: initial project structure

---
+++ src/main/java/com/urlsnap/UrlsnapApiApplication.java
@@ -0,0 +1,12 @@
+package com.urlsnap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlsnapApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlsnapApiApplication.class, args);
+    }
+}
